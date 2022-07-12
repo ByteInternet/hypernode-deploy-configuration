f8f945e8c3224af1bd9ba6c157e35fd21e48bd6e
Timon de Groot <timon.degroot@hypernode.com>
2022-07-12 12:32:04 +0200

    Undo deprecation for TaskConfigurationInterface

--- src/TaskConfigurationInterface.php
+++ src/TaskConfigurationInterface.php
@@ -2,9 +2,6 @@
 
 namespace Hypernode\DeployConfiguration;
 
-/**
- * @deprecated Task configuration is not supported on the Hypernode platform at the moment and configuration will not be taken into account
- */
 interface TaskConfigurationInterface
 {
 }
